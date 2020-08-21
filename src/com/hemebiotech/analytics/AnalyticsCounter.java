b732ac200c7aa1684e5380e649eda571868faa32
Kehor <kevindulieu.2000@gmail.com>
2020-08-21 04:44:56 +0200

    functional project - !no comment - !project not broken down

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,11 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -14,30 +19,33 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		Map<String, Integer> unsortMap = new HashMap<String, Integer>();
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			if(unsortMap.get('"'+line+'"') != null){
+				unsortMap.put( '"'+line+'"' , ( unsortMap.get('"'+line+'"') + 1) );
+			}else unsortMap.put( '"'+line+'"' , 1);
+			line = reader.readLine();
 		}
-		
+		Map<String, Integer> symptomsMap = new TreeMap<String, Integer>(unsortMap);
+		printMap(symptomsMap);
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
+		symptomsMap.forEach((k, v) -> {
+			try {
+				writer.write(k +": " + v + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.format("key: %s, value: %d%n", k, v);
+		});
 		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}
+	public static <K, V> void printMap(Map<K, V> map) {
+		for (Map.Entry<K, V> entry : map.entrySet()) {
+			System.out.println("Key : " + entry.getKey()
+					+ " Value : " + entry.getValue());
+		}
 	}
 }
